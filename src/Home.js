6a517c3847c4089128406b4903d967bc4427a8f9
DanielArellano31 <utm23090410@utma.edu.mx>
2024-11-05 10:49:41 -0600

    First commit

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import { Button, Card, Container } from 'react-bootstrap';
+
+const Home = ()=>{
+  return (
+    <Container>
+      <Card className='bg-warning' >
+        <Card.Title > PUTOS</Card.Title>
+        <Card.Body style={{color:'white'}}>
+          JAJAJAAJAJAJA
+        </Card.Body>
+      </Card>
+    </Container>
+  )
+}
+
+export default Home;
